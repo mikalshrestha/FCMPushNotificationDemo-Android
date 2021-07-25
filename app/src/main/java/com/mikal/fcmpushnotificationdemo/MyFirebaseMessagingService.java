7b5aa9cdd665b16b8b5ea474f02589ee6c56b5e3
Mikal Shrestha <mikalshrestha@Mikals-MacBook-Pro.local>
2021-07-25 19:52:47 +0545

    fcm push notification demo setup

---
+++ app/src/main/java/com/mikal/fcmpushnotificationdemo/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.mikal.fcmpushnotificationdemo;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Mikal Shrestha on 7/25/21
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
